2cd61f2ac598a84c8fee1ebb5338869439df08ae
Cat-Div7 <omarashraf11234@gmail.com>
2025-11-22 18:42:31 +0200

    Updated useLogout hook

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { STORAGE_KEY_TOKEN, LOGIN_PATH } from "@constants";
+import { STORAGE_KEY_TOKEN, STORAGE_KEY_WELCOME, LOGIN_PATH } from "@constants";
 import { toast } from "sonner";
 
 export function useLogout() {
@@ -7,6 +7,7 @@ export function useLogout() {
 
   const handleLogout = async () => {
     localStorage.removeItem(STORAGE_KEY_TOKEN);
+    localStorage.removeItem(STORAGE_KEY_WELCOME);
     toast.warning("Logging out..!");
     await new Promise((resolve) => setTimeout(resolve, 1500));
     navigate(LOGIN_PATH);
